c3b6c8257f950fd501868e2e51b77325311ef8e9
Mohamed Naim <mhnaim1@gmail.com>
2011-05-16 23:53:07 -0400

    Added a sidebar and some styling

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,29 +12,35 @@ describe UsersController do
              @user = Factory(:user)  #Instead of passing it a hash with initialization values as in [Factory(:user, :password => "mypassword", :password_confirmation => "mypassword")], it by default uses those in Factories.rb
           end  
           it "should be successful" do
-             get :show, :id => @user.id #we don't need to do this @user.id since rails is permissive and understand that implicitly
+             get :show, :id => @user  #we don't need to do this @user.id since rails is permissive and understand that implicitly
              response.should be_success
           end
           
           it "should find the right user" do
-            get :show, :id => @user.id
+            get :show, :id => @user
             assigns(:user).should == @user  
           end
       
           #tests for the user show page
           it "should have the right title" do
-            get :show, :id => @user.id
+            get :show, :id => @user
             response.should have_selector('title', :content => @user.name)
           end
     
           it "should hve the user's name" do
-            get :show, :id => @user.id
+            get :show, :id => @user
             response.should have_selector('h1', :content => @user.name)
           end
         
           it "should have a profile image" do
-            get :show, :id => @user.id
+            get :show, :id => @user
             response.should have_selector('h1>img', :class => "gravatar")
+          end
+          
+          it "should have the right URL" do
+            get :show, :id => @user
+            response.should have_selector('td>a', :content => user_path(@user),
+                                                  :href    => user_path(@user))
           end
     end
 
